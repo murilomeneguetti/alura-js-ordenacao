c6f04906d923e06d43d056d09054205fbda7c62f
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-12-22 09:10:25 -0300

    aula 2 - selection sort

--- 1_menorPreco.js
+++ 1_menorPreco.js
@@ -1,12 +1,17 @@
 const livros = require('./listaLivros');
 
-//posicao do preco mais barato
-let maisBarato = 0;
-
-for (let atual = 0; atual < livros.length; atual++) {
-    if (livros[atual].preco < livros[maisBarato].preco){
-        maisBarato = atual;
+function menorValor(arrProdutos, posicaoInicial) {
+    //posicao do preco mais barato
+    let maisBarato = posicaoInicial;
+    for (let atual = posicaoInicial; atual < arrProdutos.length; atual++) {
+        if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
+            maisBarato = atual;
+        }
     }
+    return maisBarato;
 }
 
-console.log(`O livro mais barato custa ${livros[maisBarato].preco} reais e o título é ${livros[maisBarato].titulo}`);
+//exportando para poder usar a função em outros códigos;
+module.exports = menorValor;
+
+// console.log(`O livro mais barato custa ${livros[maisBarato].preco} reais e o título é ${livros[maisBarato].titulo}`);
